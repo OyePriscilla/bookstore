24b476d8ab7dc1f552f162595a5270f1b2f351d3
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-08-17 03:32:08 +0100

    Add store state to the app functionality

--- src/Components/BookStore.js
+++ src/Components/BookStore.js
@@ -1,38 +1,23 @@
-import React, { Component } from 'react';
+import { useSelector } from 'react-redux';
+
 import AddBook from './AddBook';
 import Book from './Book';
 
-export default class BookStore extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bookList: [
-        {
-          id: 1,
-          title: 'Emotional Intelligence',
-          author: 'Samuel Oyee',
-        },
-        {
-          id: 2,
-          title: 'Oil of Gladness',
-          author: 'Daniel Great',
-        },
-        {
-          id: 3,
-          title: 'Unspeakable Joy',
-          author: 'AdeOluwa Samuel',
-        },
-      ],
-    };
-  }
+const BookStore = () => {
+  const books = useSelector((state) => state.books);
+  return (
+    <div>
+      {books.map((book) => (
+        <Book
+          key={book.id}
+          id={book.id}
+          title={book.title}
+          author={book.author}
+        />
+      ))}
+      <AddBook />
+    </div>
+  );
+};
 
-  render() {
-    const { bookList } = this.state;
-    return (
-      <div>
-        {bookList.map((book) => <Book key={book.id} title={book.title} author={book.author} />)}
-        <AddBook />
-      </div>
-    );
-  }
-}
+export default BookStore;
